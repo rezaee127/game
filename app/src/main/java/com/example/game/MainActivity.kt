33735937620891c80152a2a75c73afbf3d51502d
razie rezaee <rezaie127@yahoo.com>
2022-03-01 18:26:35 +0330

    add binding

--- app/src/main/java/com/example/game/MainActivity.kt
+++ app/src/main/java/com/example/game/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.game.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var binding:ActivityMainBinding
+         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        //setContentView(R.layout.activity_main)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        }
 }
